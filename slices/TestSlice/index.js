08dbe41292867bfc835369d0a035cb3a8e8e6eed
Elsa Katrín <elsa.katrin@gmail.com>
2023-04-23 12:34:30 +0000

    fixing typos in testpage

--- slices/TestSlice/index.js
+++ slices/TestSlice/index.js
@@ -37,12 +37,12 @@ const TestSlice = ({ slice }) => (
     Prismic serves all images through the Content Delivery Network  {"(CDN)"}  Imgix. Imgix allows image transformations using URL Parameters. Those URL parameters are provided using the Imgixparams prop. 
     </p> 
     <p className={styles.imgixab}> 
-    {"<Imgix>"} is a component for React from react-imgix library. It generates an SrcSet from the image which grants the browser the flexibility to choose the best suited image for the current window size, so it makes the images responsive!
-    So the SrcSet provides different versions of the image to the browser which the browser will select based on a few things; screen size and resolution, device pixle ratio {"(DPI)"}, sometimes bandwidth and the sizes prop from the developer.
-    Since Imgix does this automatcally, we have to spend a little less time worrying about making images responsive. Clever and comfortable! And of course there is the Imgixparams prop that we use to get a better control of the images.  
+    {"<Imgix>"} is a component for React from react-imgix library. It generates an SrcSet {"(source set)"} from the image which grants the browser the flexibility to choose the best suited image for the current window size, so it makes the images responsive!
+    So the SrcSet provides different versions of the image to the browser which the browser will select based on a few things; screen size and resolution, device pixel ratio {"(DPR)"}, sometimes bandwidth and the sizes prop from the developer.
+    Since Imgix does this automatically, we have to spend a little less time worrying about making images responsive. Clever and comfortable! And of course there is the Imgixparams prop that we use to get a better control of the images.  
     </p>
         <p className={styles.paragraph}>When using Prismic the image goes through imgix, they're automatically compressed and optimised. There's a possibility to bypass the compression from Prismic/Imgix by deleting it from the API. I've disabled it from these images, so it will be added in manually on some photos for demonstration.</p>
-        <p className={styles.paragraph}>All the images have the same sizes attribute, the sizes are used for making them responsive. We're pairing media queries with lengths and telling out browser where the breakpoints are for our website.</p>
+        <p className={styles.paragraph}>All the images have the same sizes attribute, the sizes are used for making them responsive. We're pairing media queries with lengths and telling our browser where the breakpoints are for our website.</p>
         <pre>sizes="[media query] [width above media query], [width below media query]  </pre>
         <pre>sizes="(min-width: 570px) 60vw, 80vw"</pre>
 
@@ -71,6 +71,7 @@ const TestSlice = ({ slice }) => (
       imgixParams={{ 
         auto:"compress",
         cs:'strip',
+        dpr:'0',
       }}
       /> 
    <div className={styles.paragraphwrapper}>
@@ -81,10 +82,10 @@ const TestSlice = ({ slice }) => (
         ar: "1:1",
         </pre>
     <p className={styles.paragraph}> 
-    Here I attempted add parameters to control the image. PrismicNextImage wants that we use the width and height prop, here it's set to 600x401.
-    I put the fit parameter to crop and aspect ratio  {"(ar)"} to 1x1. We can see that it is not working, it keeps its original ratio but the image stretches, we don't want that. 
-    The fit:"crop" parameter resizes the image to fit width and height or the aspect ratio parameter {"(ar:)"} and crops any excess image data. The resault is supposed to match the width and height or aspect ratio without distorting the image.
-    The width and height prop from NextPrismicImage can not be overwritten and doesn't allow the imgix paramerter to work as it should, but if we remove width and height prop from the image goes out of control and becomes huge! 
+    Here I attempted add parameters to control the image. PrismicNextImage wants us to use the width and height prop, here it's set to 600x401.
+    I put the fit parameter to crop and aspect ratio  {"(ar:)"} to 1x1. We can see that it is not working, it keeps its original ratio but the image stretches, we don't want that. 
+    The fit:"crop" parameter resizes the image to fit width and height or the aspect ratio parameter {"(ar:)"} and crops any excess image data. The result is supposed to match the width and height or aspect ratio without distorting the image.
+    The width and height prop from NextPrismicImage can not be overwritten and doesn't allow the imgix paramerter to work as it should. If we remove width and height prop from the image, it goes out of control and becomes huge because then there aren't any size restrictions applied on the image.  
     </p>
   </div>
 
@@ -110,7 +111,7 @@ const TestSlice = ({ slice }) => (
               ar: "1:1",
               </pre>
           <p className={styles.paragraph}> 
-            Here we're using a different fit parameter. Here I changed the width and height prop on the photo to 600x600 and kept on using fit:"crop" parameter. With out that parameter the image would become distorted. 
+            Here we're using a different fit parameter. Here I changed the width and height prop on the photo to 600x600 and kept on using fit:"crop" parameter. Without that parameter the image would become distorted. 
           </p>
           </div>
       <PrismicNextImage 
@@ -136,11 +137,11 @@ const TestSlice = ({ slice }) => (
         cs: 'srgb' ,
       </pre>
       <p className={styles.paragraph}> 
-        On this image the compression has been removed and auto:"format" has been added instead. The format logic will attempt to serve images in AVIF format when it's supported, if it's not it fallsback to WebP, if that's not supported either the fallback is JPG. 
+        On this image the compression has been removed and auto:"format" has been added instead. The format logic will attempt to serve images in AVIF format when it's supported, if it's not it falls back to WebP, if that's not supported either the fallback is JPG. 
         AVIF files are up to 50% lighter than JPG files and up to 30% lighter than WebP files. That means our website will also be lighter! 
-        The quality {"(q:)"} has been changed to 80, when a image is set to compress the default quality is set to 45, but with the compress removed the default becomes 75.
-        The image has been sharpened using unsharp mask {"(usm:)"} which sharpens the image details, and unsharp mask ratio {"(usmrad:)"} which determines how mnay pixles should be included to enhance the contrast when using unsharp mask. 
-        At last a colorspace {"(cs:)"} has been added. When images are compressed they lose their colorspace and tend to look gray. Even though this photo isn't compressed anymore I wanted to be sure that the image would be served in the right colorspace. 
+        The quality {"(q:)"} has been changed to 80, when an image is set to compress the default quality is set to 45, but with the compress removed the default becomes 75.
+        The image has been sharpened using unsharp mask {"(usm:)"} which sharpens the image details, and unsharp mask ratio {"(usmrad:)"} which determines how mnay pixels should be included to enhance the contrast when using unsharp mask. 
+        Finally, a colorspace {"(cs:)"} has been added. When images are compressed they lose their colourspace and tend to look gray. Even though this photo isn't compressed anymore I wanted to be sure that the image would be served in the right colourspace. 
       </p>
       </div>
 
@@ -160,9 +161,9 @@ const TestSlice = ({ slice }) => (
 {/* NextPrismicImage conclution */}
     <div className={styles.paragraphwrapper}>
       <p className={styles.paragraph}> 
-      Conclution: The photos look good! Obviously the imgixparams sizing parameters that we tried to use above didn't work. 
-      You might have noticed that the images aren't resonsive, I of course could have added CSS media queries or even layout="responsive" to the images. 
-      My problem was that the sizes attribute and srcset don't apply as effortlessy to those as to the Imgix component.
+      Conclusion: The photos look good! Obviously, the imgixparams sizing parameters that we tried to use above didn't work. 
+      You might have noticed that the images aren't responsive. I could have added CSS media queries or even layout="responsive" to the images. 
+      My problem was that the sizes attribute and srcset don't apply as effortlessly to those as to the Imgix component.
       That problem kind of defeats the purpose of having those clever srcset that Prismic provides from Imgix.</p>
   </div>
 
@@ -177,7 +178,7 @@ const TestSlice = ({ slice }) => (
           cb:"strip",
         </pre>
         <p className={styles.paragraph}>
-          This image has compression and the color space has been removed. This is added to mimic how the photo looks like when it's compressed without the API being altered.
+          This image has compression and the color space has been removed. This is added to mimic what the photo looks like when it's compressed without the API being altered.
         </p>
     </div>
         <Imgix src={slice.primary.testimage.url} 
@@ -199,11 +200,11 @@ const TestSlice = ({ slice }) => (
           cs: 'srgb' ,
         </pre>
         <p className={styles.paragraph}> 
-          On this image the compression has been removed and auto:"format" has been added instead. The format logic will attempt to serve images in AVIF format when it's supported, if it's not it fallsback to WebP, if that's not supported either the fallback is JPG. 
-          AVIF files are up to 50% lighter than JPG files and up to 30% lighter than WebP files. That means our website will also be lighter! 
-          The quality {"(q:)"} has been changed to 80, when a image is set to compress the default quality is set to 45, but with the compress removed the default becomes 75.
-          The image has been sharpened using unsharp mask {"(usm:)"} which sharpens the image details, and unsharp mask ratio {"(usmrad:)"} which determines how mnay pixles should be included to enhance the contrast when using unsharp mask. 
-          At last a colorspace {"(cs:)"} has been added. When images are compressed they lose their colorspace and tend to look gray. Even though this photo isn't compressed anymore I wanted to be sure that the image would be served in the right colorspace. 
+        On this image the compression has been removed and auto:"format" has been added instead. The format logic will attempt to serve images in AVIF format when it's supported, if it's not it falls back to WebP, if that's not supported either the fallback is JPG. 
+        AVIF files are up to 50% lighter than JPG files and up to 30% lighter than WebP files. That means our website will also be lighter! 
+        The quality {"(q:)"} has been changed to 80, when an image is set to compress the default quality is set to 45, but with the compress removed the default becomes 75.
+        The image has been sharpened using unsharp mask {"(usm:)"} which sharpens the image details, and unsharp mask ratio {"(usmrad:)"} which determines how mnay pixels should be included to enhance the contrast when using unsharp mask. 
+        Finally, a colorspace {"(cs:)"} has been added. When images are compressed they lose their colourspace and tend to look gray. Even though this photo isn't compressed anymore I wanted to be sure that the image would be served in the right colourspace. 
         </p>
       </div>
        <Imgix 
@@ -259,11 +260,12 @@ const TestSlice = ({ slice }) => (
           cs: 'srgb',
         </pre>
         <p className={styles.paragraph}> 
-          Can you see the quality difference on this one? Device pixle ratio or the DPR as added to this image. It controls the output density of your image. 
+          Can you see the quality difference on this one? Device pixel ratio or the DPR as added to this image. It controls the output density of your image. 
           You can put DPR in the 'sizes' prop and then the browser can serve images at the correct density for each device. 
           This makes images faster and saves bandwidth for users with lower-DPR devices, while delivering the expected crispness of high-DPR imagery to those devices.
-          But because this parameter is on this photo, it would always display as DRP:"2". That wouldn't be good for all devices. I recommend opening this image in a new tab, changing the dpr="" in the url and see the difference when you change it!  
-          Higher DPR makes the picture bigger of course so the sizes attribute had to be changed. 
+          But because this parameter is on this photo, it would always display as DRP:"2". That wouldn't be good for all devices, like a phone or very large screens. 
+          I recommend opening this image in a new tab, changing the dpr="" in the url and see the difference when you change it!  
+          Higher DPR makes the picture bigger so the sizes attribute had to be changed. 
         </p>
         <pre>sizes="(min-width: 570px) 35vw, 40vw"</pre>
          </div>
@@ -292,8 +294,8 @@ const TestSlice = ({ slice }) => (
           vib: '10',
         </pre>
         <p className={styles.paragraph}> 
-              For the last one I added auto:'enhance'. The image is adjusted using the distribution of highlights, midtones, and shadows across all three channels—red, green, and blue {"RGB"}
-              It gives the image a bit more vibrant appearance. I also added a bit of vibrance just to make the colors pop more. 
+              For the last one I added auto:'enhance'. The image is adjusted using the distribution of highlights, midtones, and shadows across all three channels—red, green, and blue {"RGB"}.
+              It gives the image a bit more vibrant appearance. I also added a bit of vibrance just to make the colors pop more.
         </p>
          </div>
         <Imgix 
@@ -313,9 +315,9 @@ const TestSlice = ({ slice }) => (
 
         <div className={styles.paragraphwrapper}>
       <p className={styles.paragraph}> 
-      Conclution: Imgix has a lot of fun parameters, a lot more than I have put in here. When I started to figure out how it worked I began to like working with it. 
-      I think that the srcset in the imgix component are very comfortable to use and a clever technology. It's possible to use with other image components and make the scrsets manually but making the imgix component do it for you is so nice. 
-      I also think that using this way with Prismic is very convenient because the client doesn't have to think about what size picture they upload, the code and the src sets does it for them. 
+      Conclusion: Imgix has a lot of fun parameters, a lot more than I have put in here. Once I got the hang of it I really saw the practicality of it.  
+      I think that the srcset in the imgix component are very comfortable to use and a clever technology. It's possible to use with other image components and make the scrsets manually, but the imgix component does it for us, which is super nice. 
+      Also using the imgix component with Prismic is very convenient, because the client doesn't have to think about what size picture they upload, the code and the srcsets does it for them. 
       </p>
   </div>
 </div>
